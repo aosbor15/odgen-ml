fe08c3ca42b8cefda7ee6dc8a808add7c6f90364
Alex Osborne <aosbor15@jhu.edu>
2023-04-28 11:45:52 -0400

    first commit

---
+++ testPandas.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import sys
+
+if len(sys.argv) < 2:
+	print("Usage: python3 testPandas.py <filename>")
+	sys.exit(1)
+
+filename = sys.argv[1]
+
+node_df = pd.read_table('nodes.csv', header=0)
+#adj_list = {id: [] for id in node_df['id:ID']}
+edge_df = pd.read_table('rels.csv', header=0)
+#for i, row in edge_df.iterrows():
+    #src_node = row['start:START_ID']
+    #dest_node = row['end:END_ID']
+    #adj_list[src_node].append(dest_node)
+#adj_df = pd.DataFrame({'id:ID': adj_list.keys(), 'neighbors': adj_list.values()})
+#graph_df = adj_df.merge(node_df, left_index=True, right_on='id:ID')
+#graph_df = graph_df.drop(columns=['id:ID_x', 'id:ID_y'])
+#graph_df = graph_df.explode('neighbors')
+#graph_df.to_csv(filename, index=False)
+
+graph2 = edge_df.merge(node_df, left_index=True, right_on='id:ID')
+#print(graph2[['start:START_ID', 'end:END_ID', 'type', 'name', 'type:TYPE']])
+graph2 = graph2.drop(columns=['var', 'taint_src', 'taint_dst', 'id:ID'])
+graph2 = graph2.rename(columns={'start:START_ID':'source', 'end:END_ID':'dest', 'type':'source_type', 'name':'source_name', 'type:TYPE':'edge_type'})
+graph2.to_csv('graph_files/' + filename, index=False)
---
+++ testPyTorch.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import sys
+import torch
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, TensorDataset
+from torch.nn.functional import binary_cross_entropy_with_logits
+from torch.optim import Adam
+from torch.nn import Linear, ReLU, Sequential, Sigmoid
+
+if len(sys.argv) < 2:
+    print("Usage: python3 testPyTorch.py <filename>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+# Load graph dataframe
+graph_df = pd.read_csv('graph_files/' + filename)
+#graph_df.fillna(0, inplace=True)
+graph_df.fillna({'tainted': False}, inplace=True)
+graph_df.fillna({'lineno:int': -1, 'endlineno:int': -1, 'childnum:int': -1}, inplace=True)
+le = LabelEncoder()
+graph_df['edge_type'] = le.fit_transform(graph_df['edge_type'])
+graph_df['source_type'] = le.fit_transform(graph_df['source_type'])
+#graph_df['code'] = le.fit_transform(graph_df['code'])
+#looks like this is NaN for almost every entry?
+#graph_df['classname'] = le.fit_transform(graph_df['classname'])
+graph_df['source_name'] = le.fit_transform(graph_df['source_name'])
+#this is not a relevant feature
+#graph_df['export'] = le.fit_transform(graph_df['export'])
+graph_df.to_csv('processed_csv/p-'+filename, index=False)
+
+# Define features and target
+#features = graph_df[['source', 'dest', 'edge_type', 'source_name', 'source_type', 'lineno:int', 'endlineno:int', 'childnum:int']].astype('float32').to_numpy()
+features = graph_df[['source', 'dest', 'source_name', 'source_type']].astype('float32').to_numpy()
+
+target = graph_df['tainted'].astype('float32').to_numpy()
+# Split the data into training and validation sets
+X_train, X_val, y_train, y_val = train_test_split(features, target, test_size=0.2)
+
+# Convert the data to PyTorch tensors and create a DataLoader
+train_data = TensorDataset(torch.from_numpy(X_train), torch.from_numpy(y_train))
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+
+# Define the neural network model
+model = Sequential(
+    Linear(features.shape[1], 16),
+    ReLU(),
+    Linear(16, 8),
+    ReLU(),
+    Linear(8, 1),
+    Sigmoid()
+)
+
+# Define the loss function and optimizer
+loss_fn = binary_cross_entropy_with_logits
+optimizer = Adam(model.parameters(), lr=0.001)
+
+# Train the model
+num_epochs = 10
+for epoch in range(num_epochs):
+    for X_batch, y_batch in train_loader:
+        optimizer.zero_grad()
+        y_pred = model(X_batch).squeeze()
+        loss = loss_fn(y_pred, y_batch)
+        loss.backward()
+        optimizer.step()
+
+    with torch.no_grad():
+        y_val_pred = model(torch.Tensor(X_val)).squeeze()
+        val_loss = loss_fn(y_val_pred, torch.Tensor(y_val))
+        print(f"Epoch {epoch+1}, val_loss: {val_loss.item():.4f}")
